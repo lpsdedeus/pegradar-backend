1784ae95907af3a74a1797b5bfc4e91ff44d12fb
lpsdedeus <lpsdedeus@gmail.com>
2025-07-22 01:37:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const { runMonitor } = require('./service/arbitrageMonitor');
+const arbitrageMonitor = require('./services/arbitrageMonitor');
 
 dotenv.config();
 
